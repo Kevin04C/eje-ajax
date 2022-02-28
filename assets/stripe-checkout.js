65ff3963c0d0d5419b0b65e791d2c83f35b535a9
Kevin <kevincespedes2004@gmail.com>
2022-02-28 18:27:37 -0500

    new improvements

--- assets/stripe-checkout.js
+++ assets/stripe-checkout.js
@@ -1,3 +1,4 @@
+import stripeKeys from "./stripe-keys.js";
 import STRIPE_KEYS from "./stripe-keys.js";
 // console.log(STRIPE_KEYS);
 
@@ -11,6 +12,7 @@ const fetchOptions = {
 };
 let products;
 let prices;
+const moneyFormat = (num) => `S/ ${num.slice(0, -2)},${num.slice(-2)}`;
 Promise.all([
   fetch("https://api.stripe.com/v1/products", fetchOptions),
   fetch("https://api.stripe.com/v1/prices", fetchOptions),
@@ -28,13 +30,11 @@ Promise.all([
       template.querySelector(".taco").dataset.price = price.id;
       template.querySelector("img").src = productData.images[0];
       template.querySelector("img").alt = productData.name;
-      template.querySelector("figcaption").innerHTML = `<p>Producto: ${
-        productData.name
-      }</p> <p> Precio: S/ 
-      ${price.unit_amount_decimal.slice(
-        0,
-        -2
-      )},00 ${price.currency.toUpperCase()}</p>`;
+      template.querySelector("figcaption").innerHTML = `
+      ${productData.name}
+      <br>
+      ${moneyFormat(price.unit_amount_decimal)}
+      `;
       const clone = document.importNode(template, true);
       fragment.appendChild(clone);
     });
@@ -43,3 +43,21 @@ Promise.all([
   .catch((err) => {
     console.log(err);
   });
+document.addEventListener("click", (e) => {
+  if (e.target.matches(".taco *")) {
+    let dataPrice = e.target.parentElement.getAttribute("data-price");
+    Stripe(stripeKeys.public)
+      .redirectToCheckout({
+        lineItems: [{ price: dataPrice, quantity: 1 }],
+        mode: "subscription",
+        successUrl: "http://127.0.0.1:5500/assets/stripe-success.html",
+        cancelUrl: "http://127.0.0.1:5500/assets/cancel.html",
+      })
+      .then((res) => {
+        console.log(res);
+        if (res.error) {
+          tacos.insertAdjacentHTML("afterend", res.error.message);
+        }
+      });
+  }
+});
